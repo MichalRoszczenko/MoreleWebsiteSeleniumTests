46bd1c2171d35d0fcbc4cf1643a1d5f00530d4ba
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-10-30 14:05:45 +0100

    Created AddProductsToBasket test

--- MoreleSeleniumWebsiteTests/AddProductsToBasketTests.cs
+++ MoreleSeleniumWebsiteTests/AddProductsToBasketTests.cs
@@ -1,8 +1,11 @@
 using FluentAssertions;
 using MoreleSeleniumFramework.Driver;
+using MoreleSeleniumFramework.Models;
 using MoreleSeleniumFramework.Pages.Interfaces;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
 
 namespace MoreleSeleniumWebsiteTests
 {
@@ -29,25 +32,65 @@ namespace MoreleSeleniumWebsiteTests
 		[InlineData(13)]
 		[InlineData(5)]
 		[InlineData(29)]
-		public void AddProductToBasket_AddedProductShouldBeInBasket(int productNumber)
+		public void AddProductToBasket_AddedGraphicCardShouldBeInBasket(int productNumber)
 		{
 			//act
-			_wait.Until(d => _homePage.AcceptCookiesButtons.Displayed);
-			_homePage.AcceptCookiesButtons.Click();
-			_actions.MoveToElement(_homePage.ComputerComponentsMainCategory).Perform();
-			_homePage.GraphicCardsSubCategory.Click();
+			PreliminarySetup();
 
-			var products = _productsInCategory.Products;
-			var product = _productsInCategory.GetProductInfo(products, productNumber);
-			_productsInCategory.AddProductToBasket(products, productNumber);
-			_wait.Until(d => _productsInCategory.WarrantyPopUp.Displayed);
-			_productsInCategory.WarrantyWindowCloseButton.Click();
+			var product = AddProductToBasket(_productsInCategory.Products, productNumber);
 
 			_homePage.Basket.Click();
 			var productsInBasket = _basketPage.GetProductsFromBasket(_basketPage.ProductsInBasket);
 
 			//assert
 			productsInBasket.Should().ContainEquivalentOf(product);
+		}
+
+		[Theory]
+		[InlineData(25,4)]
+		public void AddProductsToBasket_AddedGraphicCardShouldBeInBasket(int startProduct, int productsToAdd)
+		{
+			//assert
+			List<ProductDetails> addedProducts = new List<ProductDetails>();
+
+			//act
+			PreliminarySetup();
+
+			var products = _productsInCategory.Products;
+
+			for (int i = startProduct; i < startProduct+productsToAdd; i++)
+			{
+				var product = AddProductToBasket(products,i);
+
+				addedProducts.Add(product);
+			}
+
+			_homePage.Basket.Click();
+			var productsInBasket = _basketPage.GetProductsFromBasket(_basketPage.ProductsInBasket);
+
+			//assert
+			foreach (var item in addedProducts)
+			{
+				productsInBasket.Should().ContainEquivalentOf(item);
+			}
+		}
+
+		private void PreliminarySetup()
+		{
+			_wait.Until(d => _homePage.AcceptCookiesButtons.Displayed);
+			_homePage.AcceptCookiesButtons.Click();
+			_actions.MoveToElement(_homePage.ComputerComponentsMainCategory).Perform();
+			_homePage.GraphicCardsSubCategory.Click();
+		}
+
+		private ProductDetails AddProductToBasket(ReadOnlyCollection<IWebElement> products, int productNumber)
+		{
+			var product = _productsInCategory.GetProductInfo(products, productNumber);
+			_productsInCategory.AddProductToBasket(products, productNumber);
+			_wait.Until(d => _productsInCategory.WarrantyPopUp.Displayed);
+			_productsInCategory.WarrantyWindowCloseButton.Click();
+
+			return product;
 		}
 	}
 }
