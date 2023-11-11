fc9c3d259a8065cb9e463e6195272678d881d133
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-11-11 17:11:10 +0100

    Implemented filtering interface

--- MoreleSeleniumFramework/Pages/Interfaces/IProductsInCategoryPage.cs
+++ MoreleSeleniumFramework/Pages/Interfaces/IProductsInCategoryPage.cs
@@ -1,16 +1,18 @@
 ﻿using MoreleSeleniumFramework.Models;
+using MoreleSeleniumFramework.Pages.Interfaces.Minor;
 using OpenQA.Selenium;
 using System.Collections.ObjectModel;
 
 namespace MoreleSeleniumFramework.Pages.Interfaces
 {
-    public interface IProductsInCategoryPage
-    {
+    public interface IProductsInCategoryPage : IProductFiltering
+	{
 		IWebElement WarrantyPopUp { get; }
 		IWebElement WarrantyWindowCloseButton { get; }
 		ReadOnlyCollection<IWebElement> ProductsProvidedByMorele { get; }
 		ReadOnlyCollection<IWebElement> ProductsInCategory { get; }
 		void AddProductToBasket(ReadOnlyCollection<IWebElement> productsList, int productNumber);
 		ProductDetails GetProductInfo(ReadOnlyCollection<IWebElement> productsList, int productNumber);
+		IWebElement SelectPageNumber(int pageNumber);
 	}
 }
--- MoreleSeleniumFramework/Pages/ProductsInCategoryPage.cs
+++ MoreleSeleniumFramework/Pages/ProductsInCategoryPage.cs
@@ -24,17 +24,22 @@ public sealed class ProductsInCategoryPage : IProductsInCategoryPage
 	public IWebElement WarrantyWindowCloseButton => _driver.FindElement(By.ClassName("md-header-close-btn"));
 	public ReadOnlyCollection<IWebElement> ProductsProvidedByMorele => 
 		_driver.FindElements(By.CssSelector("div[class='cat-product card']:not(div:has([data-controller='merchant-info']))"));	
-	
 	public ReadOnlyCollection<IWebElement> ProductsInCategory => 
 		_driver.FindElements(By.CssSelector("div[class='cat-product card']"));
 
+	public ReadOnlyCollection<IWebElement> ProducersFilter =>
+		_driver.FindElements(By.CssSelector("div[data-fhead-name='Producenci'] div[data-name]"));
+	public IWebElement ProducersFilterShowMoreButton =>
+		_driver.FindElement(By.CssSelector("div[data-fhead-name='Producenci'] span[data-trans-open]"));
+
+	public IWebElement SelectPageNumber(int pageNumber) => 
+		_driver.FindElement(By.CssSelector($".pagination.dynamic a[data-page='{pageNumber}']"));
 	public void AddProductToBasket(ReadOnlyCollection<IWebElement> productsList, int productNumber)
 	{
 		IWebElement element = productsList[productNumber].FindElement(By.ClassName("cat-product-buttons"));
 		_actions.MoveToElement(element).Perform();
 		element.Click();
 	}
-
 	public ProductDetails GetProductInfo(ReadOnlyCollection<IWebElement> productsList, int productNumber)
 	{
 		IWebElement element = productsList[productNumber];
@@ -44,4 +49,16 @@ public sealed class ProductsInCategoryPage : IProductsInCategoryPage
 
 		return new ProductDetails() { Name = name, Price = price };
 	}
+
+	public IWebElement SelectFiltratingByProducer(string producerName)
+	{
+		var producers = ProducersFilter;
+
+		IWebElement producer = producers
+			.First(x => x.GetAttribute("data-name").ToLower() == producerName.ToLower());
+
+		return producer;
+	}
+	public IWebElement ProducerFilterCheckbox(IWebElement producerFilter) => producerFilter.FindElement(By.CssSelector("label"));
+	public int NumberOfFilteredProducts(IWebElement filter) => int.Parse(filter.GetAttribute("data-count"));
 }
