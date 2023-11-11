29a3557f51e1b7a77ed49f359154b3b16db2477a
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-11-11 17:40:27 +0100

    Created WaitFor class

---
+++ MoreleSeleniumFramework/Utilities/WaitFor.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+
+namespace MoreleSeleniumFramework.Utilities;
+
+public static class WaitFor
+{
+	public static Func<IWebDriver,bool> ElementInvisibility(IWebElement webElement)
+	{
+		return delegate (IWebDriver driver)
+		{
+			try
+			{
+				return !webElement.Displayed;
+			}
+			catch (NoSuchElementException)
+			{
+				return true;
+			}
+			catch (StaleElementReferenceException)
+			{
+				return true;
+			}
+		};
+	}
+}
