204a66929cce6c16ba2d12622e0aa3ecdd2da20e
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-11-11 17:09:24 +0100

    Created filtering interface

---
+++ MoreleSeleniumFramework/Pages/Interfaces/Minor/IProductFiltering.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace MoreleSeleniumFramework.Pages.Interfaces.Minor;
+
+    public interface IProductFiltering
+	{
+		IWebElement ProducersFilterShowMoreButton { get; }
+		IWebElement SelectFiltratingByProducer(string producerName);
+		IWebElement ProducerFilterCheckbox(IWebElement producerFilter);
+		int NumberOfFilteredProducts(IWebElement filter);
+	}
